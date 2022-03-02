4194d2f882ab11d938cba6bb55d7ce83603f7dce
Player Ghost <sousaitalo12@gmail.com>
2022-03-02 18:48:04 -0300

    Update

--- configuration.py
+++ configuration.py
@@ -1,9 +1,11 @@
 import yaml
 
-class Configuration:
-  with open('./config.yaml','r',encoding='utf-8') as open_yml:
-    c = yaml.safe_load(open_yml)
 
-  threshold = c['threshold']
-  home = c['home']
-  telegram = c['telegram']
+class Configuration:
+    with open('./config.yaml', 'r', encoding='utf-8') as open_yml:
+        c = yaml.safe_load(open_yml)
+
+    threshold = c['threshold']
+    home = c['home']
+    telegram = c['telegram']
+    accountLabels = c['accountLabels']
